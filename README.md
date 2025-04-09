35f3c0c2e0c912a10c4310f1f74504c962368e86
Sundar-black <sundarbaskar2411@gmail.com>
2025-04-09 11:06:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project
+# Class-9
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var box;
+
+function setup() {
+  createCanvas(1920,1920);
+  box = createSprite(400,400,60,10);
+
+}
+
+function draw() 
+{
+   background(148, 3, 252);
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+    box.shapeColor=color(252, 231, 3);
+    
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    box.shapeColor=color(3, 53, 252);
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+    box.shapeColor=color(252, 3, 206);
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+    box.shapeColor=color(252, 3, 20);
+  }
+
+  drawSprites();
+}
+
+
+
+
